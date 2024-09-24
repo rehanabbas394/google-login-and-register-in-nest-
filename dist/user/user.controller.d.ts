a5584902b3943c592f072145a0c86affc283896e
rehanabbas394 <seorehan1122@gmail.com>
2024-09-24 12:16:59 +0500

    initial commit

--- dist/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsersService } from './user.service';
-import { User } from './entity/user.entity';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    findOneByEmail(email: string): Promise<User>;
-    create(userData: Partial<User>): Promise<User>;
-}
--- dist/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './entity/user.entity';
-export declare class UsersService {
-    private usersRepository;
-    constructor(usersRepository: Repository<User>);
-    findOneByEmail(email: string): Promise<User | undefined>;
-    create(userData: Partial<User>): Promise<User>;
-}
